1c6f88102f92676bc3a2259b923bb77de22f9a68
Aaush <89632032+Aaush-Raj@users.noreply.github.com>
2022-01-26 16:08:52 +0530

    Crud Operations using Django Forms

--- CRUD_OPERATIONS/urls.py
+++ CRUD_OPERATIONS/urls.py
@@ -1,13 +1,10 @@
 from django.contrib import admin
 from django.urls import path,include
-from . import views
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('employee/',include('Employee_Register.urls')),
-    path('', views.index,name="index"),
-    path('bot_search/', views.bot_search,name='bot_search')
     
 ]
                                    
