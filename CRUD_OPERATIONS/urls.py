3f8d8f0731a4d21aeebfcd5af791cafe8dd04daa
Aaush Raj <89632032+Aaush-Raj@users.noreply.github.com>
2022-01-26 15:35:38 +0530

    Update urls.py

--- CRUD_OPERATIONS/urls.py
+++ CRUD_OPERATIONS/urls.py
@@ -5,7 +5,7 @@ from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('employee/',include('Employee_Register.urls')),
+    #path('employee/',include('Employee_Register.urls')),
     path('', views.index,name="index"),
     path('bot_search/', views.bot_search,name='bot_search')
     
